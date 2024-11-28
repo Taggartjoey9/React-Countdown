1fa6bf053b0a1489714973acac1c7905e23a672b
Taggartjoey9 <taggartjoey9@gmail.com>
2024-11-27 20:02:16 -0500

    adding useRef hook to user name input

--- Countdown/src/components/Player.jsx
+++ Countdown/src/components/Player.jsx
@@ -1,10 +1,21 @@
+import { useState, useRef } from 'react';
+
 export default function Player() {
+
+  const [ typedName, setTypedName ] = useState(null);
+
+  const updatedName = useRef();
+
+  function handleName() {
+    setTypedName(updatedName.current.value)
+  }
+
     return (
       <section id="player">
-        <h2>Welcome unknown entity</h2>
+        <h2>Welcome {typedName ?? 'Unknown User'}</h2>
         <p>
-          <input type="text" />
-          <button>Set Name</button>
+          <input ref={updatedName} type="text" />
+          <button onClick={handleName}>Set Name</button>
         </p>
       </section>
     );
